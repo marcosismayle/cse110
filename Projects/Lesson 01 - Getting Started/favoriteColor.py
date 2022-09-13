c6506262970b559e4062db843c783d09233cce17
marcosismayle <marcosismayle@gmail.com>
2022-09-13 11:02:21 -0300

    lesson 01 concluded

---
+++ Projects/Lesson 01 - Getting Started/favoriteColor.py
@@ -0,0 +1,6 @@
+name = input('Hey user! What is your name?')
+print('Hello',name,'!')
+print('Nice to meet you.')
+color = input('What is your favorite color?')
+print('Cool',name,'!')
+print('I think',color,'is a beautiful color.')
